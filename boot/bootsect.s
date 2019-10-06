8e2e9266c2c431acde6c6faa87f06b39cf1ba427
andromedax7 <andromeda7@126.com>
2019-10-06 19:01:27 +0800

    bootsect test

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -8,40 +8,29 @@ begdata:
 begbss:
 .text
 .equ BOOTSEG,0x07c0
-.equ SETUPSEG,0x9020
-.equ INITSEG,0x9000
-.equ SETUPLEN,4
-.equ SYSSEG,0x1000
-.equ ROOT_DEV ,0x301
-
-	ljmp $BOOTSEG,$_start
+ljmp $BOOTSEG,$_start
 
 _start:
-	mov $BOOTSEG,%ax
-	mov %ax,%ds
-	mov $INITSEG,%ax
-	mov %ax,%es
-	mov $256,%cx
-	sub %si,%si
-	sub %di,%di
-	rep
-	movsw
-	ljmp $INITSEG,$go
-go:
-	mov %cs ,%ax
-	mov %ax,%ds
-	mov %ax,%es
-#put stack at 0x9ff00
-	mov %ax,%ss
-	mov $0xff00,%sp
+    mov $BOOTSEG,%ax
+    mov %ax,%es
+    mov $0x03,%ah
+    xor %bh,%bh
+    int $0x10
 
+    mov $msg1,%bp
+    mov $7,%cx
+    mov $0x1301,%ax
+    mov $0x0001,%bx
+    int $0x10
 
-msg1:	.byte 13,10
-	.ascii "hello tokra
-	.byte 13,10,13,10"
-	.org 508
-root_dev:
-	.word ROOT_DEV
+msg1:
+.byte 13,10
+.ascii "12345"
+.byte 13,10,13,10
+
+.org 508
+
+boot_dev:
+    .word 0x301
 boot_flag:
-	.word 0xAA55
-
+    .word 0xaa55
